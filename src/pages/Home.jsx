0f6c995dcb6acf71264061efbcde91f485b8dae5
Felipe Moraes <felipemoraes920@gmail.com>
2025-05-06 17:25:21 -0300

    my site

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HeroSection from "../components/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-900 text-white">
+      <HeroSection />
+    </main>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+  },
+};
